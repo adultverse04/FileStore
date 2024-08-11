7f7ac5e696b18a2380cc51cee4d5382d186cf614
adultverse04 <adult_verse04@yahoo.com>
2024-08-11 12:06:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ OWNER = os.environ.get("OWNER", "Obito")
 #OWNER ID
 OWNER_ID = int(os.environ.get("OWNER_ID", "6283520099"))
 #Port
-PORT = os.environ.get("PORT", "8030")
+PORT = os.environ.get("PORT", "8080")
 #Database
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://akashmondal317689:tFW9FDQXA0zEvLPo@cluster0.qspl11g.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "Directbot")
